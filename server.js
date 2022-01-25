8a49e607e54108b080c4f0852ab74b2110da379f
autumnchris <autumncbullard@gmail.com>
2022-01-25 13:25:05 -0500

    Connect to MongoDB database

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGO_URI);
 
 app.use(express.static(`${__dirname}/public`));
 
