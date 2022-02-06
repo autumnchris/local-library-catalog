bbd749d8b8ca7c77621dedd5683a862d4359bca4
autumnchris <autumncbullard@gmail.com>
2022-02-05 22:15:03 -0500

    Fix connection to database uri

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 app.use(express.static(`${__dirname}/public`));
 
