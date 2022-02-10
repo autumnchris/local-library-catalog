3c23fd8606cf3e9bfe1c9ad284146e4286eeff26
autumnchris <autumncbullard@gmail.com>
2022-02-10 17:26:48 -0500

    Add a 404 page

--- server.js
+++ server.js
@@ -22,4 +22,8 @@ app.use(express.static(`${__dirname}/public`));
 app.use('/', indexRouter);
 app.use('/catalog', catalogRouter);
 
+app.use((req, res, next) => {
+    res.status(404).render('404', {page: 'Page not found'});
+});
+
 app.listen(port, console.log(`Server is listening at port ${port}.`));
